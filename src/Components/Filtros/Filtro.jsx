58e2ad6f5b192781347894733dec785b35fbbb13
Gabriel Dos Santos Silva <gabrieldos55570000@gmail.com>
2022-06-17 16:46:27 -0300

    parte inicial do filtro

--- src/Components/Filtros/Filtro.jsx
+++ src/Components/Filtros/Filtro.jsx
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
-import {StyledFilter} from "./StyledFiltros"
+import { StyledFilter } from "./StyledFiltros"
 
 export class Filter extends Component {
   render() {
+    state = {
+      buscaNome: "aqui recebe o texto do input",
+      precoMin: "recebe preco min do input",
+      precoMax: "recebe //",
+      valueOrdem: 1
+    }
+
+    modificaNome=(event) =>{
+      this.setState({
+        buscaNome: event.terget.value
+      })
+    }
+
+    modificaMin=(event) =>{
+      this.setState({
+        precoMin: event.terget.value
+      })
+    }
+
+    modificaMax=(event) =>{
+      this.setState({
+        precoMax: event.terget.value
+      })
+    }
+
     return (
-        <StyledFilter>
+      <StyledFilter>
         <h1>Filtro</h1>
         <div>
-          <input placeholder="Max"/>
-          <input placeholder="Min"/>
-          <input placeholder="Todos"/>
+          <input 
+          placeholder="Max" 
+          onChange={modificaNome}
+          />
+          <input placeholder="Min" 
+          onChange={modificaMin}
+          />
+          <input placeholder="Todos" 
+          onChange={modificaMax}
+          />
         </div>
       </StyledFilter>
     )
